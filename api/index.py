fb97e9f9afbd5546235a2f6b1f16ea2c0dce4b7f
Hamza Benzaoui <hamza.benzaoui@ensia.edu.dz>
2024-01-12 16:47:49 +0100

    Update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Load the pre-trained model
+model_filename = 'final_model.joblib'
+loaded_model = joblib.load(model_filename)
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the input data from the JSON request
+        data = request.get_json()
+
+        # Ensure all required features are present
+        required_features = ['TEMPERATURE', 'RH', 'WS',
+                             'RAIN', 'FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'FWI']
+        if not all(feature in data for feature in required_features):
+            return jsonify({'error': 'Missing required features'}), 400
+
+        # Prepare the input data for prediction
+        input_data = [[data[feature] for feature in required_features]]
+
+        # Make predictions using the loaded model
+        prediction = loaded_model.predict(input_data)
+
+        # Return the prediction as JSON
+        return jsonify({'prediction': prediction.tolist()}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
